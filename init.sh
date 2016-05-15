506a6770ddba2b7de3110c4ceb43082888afbac6
nikolnikon <nikolnikon@gmail.com>
2016-05-15 23:19:54 +0300

    initial commit

---
+++ init.sh
@@ -0,0 +1,7 @@
+sudo rm -r /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+
+sudo rm -r /etc/gunicorn.d/*
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
