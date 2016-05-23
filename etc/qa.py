39926740e38d480970919626173fd4c0ff7a170a
Nikolay Nikonov <nikolnikon@gmail.com>
2016-05-23 18:47:10 +0300

    Подправил конфиги nginx b gunicorn

---
+++ etc/qa.py
@@ -0,0 +1,9 @@
+CONFIG = {
+    'mode': 'django',
+            'working_dir': '/home/box/web/ask',
+            'args': (
+                '--bind=0.0.0.0:8000',
+                '--log-level=debug',
+                'ask',
+            ),
+}
